7b4861a9e0aac5743816bb9270991e47c60350a4
veles1968 <dimce.tumbov@gmail.com>
2022-01-16 13:44:59 +0100

    16.01.2022 002

--- app/config/settings.js
+++ app/config/settings.js
@@ -2,8 +2,9 @@ import Constants from "expo-constants";
 
 const settings = {
   dev: {
-    apiUrl: "http://192.168.178.30:9000/api", // LAN Karlsruhe
-    // apiUrl: "http://192.168.178.29:9000/api", // WLAN Karlsruhe
+    // apiUrl: "http://192.168.178.30:9000/api", // LAN Karlsruhe
+    // apiUrl: "http://localhost:9000/api", // LAN Karlsruhe
+    apiUrl: "http://192.168.178.29:9000/api", // WLAN Karlsruhe
     // apiUrl: "http://192.168.178.21:9000/api", // WLAN Köln
     // apiUrl: "http://192.168.43.76:9000/api", // WLAN Tethering Samsung Galaxy Note 9
     // apiUrl: "http://158.101.173.191:9000/api/",
